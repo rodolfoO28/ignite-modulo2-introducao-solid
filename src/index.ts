60341ec38321eafe0e342cd39ad380ca74b8c823
Rodolfo Oliveira <rdf2806@gmail.com>
2021-06-15 18:36:30 -0300

    Add api documentation

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import swaggerUI from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerJSON from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
 
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJSON));
 app.use("/users", usersRoutes);
 
 export { app };
